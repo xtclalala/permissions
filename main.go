5aecffc1e03926251d1664669cf656c0855b87e9
xtclalala <1197046141@qq.com>
2022-07-29 22:37:46 +0800

    去掉ws

--- main.go
+++ main.go
@@ -5,22 +5,23 @@ package main
 //go:generate go mod download
 
 import (
+	"gorm.io/gorm"
 	"permissions/global"
 	initServe2 "permissions/initServe"
 )
 
 func main() {
 	global.Viper = initServe2.InitConfig()
-	//global.Db = initServe2.InitDb()
-	//if global.Db != nil {
-	//	initServe2.InitTables(global.Db)
-	//
-	//	defer func(db *gorm.DB) {
-	//		sqlDb, err := db.DB()
-	//		if err != nil {
-	//			sqlDb.Close()
-	//		}
-	//	}(global.Db)
-	//}
+	global.Db = initServe2.InitDb()
+	if global.Db != nil {
+		initServe2.InitTables(global.Db)
+
+		defer func(db *gorm.DB) {
+			sqlDb, err := db.DB()
+			if err != nil {
+				sqlDb.Close()
+			}
+		}(global.Db)
+	}
 	initServe2.RunWindowServer()
 }
